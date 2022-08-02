d7a3d075c21982bfcae50d29003ebf2649ee0449
Rishaan Desai <77017806+Blackhole1123@users.noreply.github.com>
2022-08-01 20:13:49 -0700

    Added a LOT of descriptions

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,12 +14,14 @@ import {
 	CompletionItemKind,
 	TextDocumentPositionParams,
 	TextDocumentSyncKind,
-	InitializeResult
+	InitializeResult,
+	VersionedTextDocumentIdentifier
 } from 'vscode-languageserver/node';
 
 import {
 	TextDocument
 } from 'vscode-languageserver-textdocument';
+import * as vscode from 'vscode';
 
 // Create a connection for the server, using Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
@@ -143,7 +145,7 @@ async function validateTextDocument(textDocument: TextDocument): Promise<void> {
 	const pattern = /\b[A-Z]{2,}\b/g;
 	let m: RegExpExecArray | null;
 
-	let problems = 0;
+	const problems = 0;
 	const diagnostics: Diagnostic[] = [];
 	/*while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
 		problems++;
@@ -198,124 +200,124 @@ connection.onCompletion(
 			{
 				label: 'var',
 				kind: CompletionItemKind.Text,
-				data: 3
+				data: 'var'
 			},
 			{
 				label: 'f16',
 				kind: CompletionItemKind.Text,
-				data: 2
+				data: 'f16'
 			},
 			{
 				label: 'f32',
 				kind: CompletionItemKind.Text,
-				data: 2
+				data: 'f32'
 			},
 			{
 				label: 'f64',
 				kind: CompletionItemKind.Text,
-				data: 2
+				data: 'f64'
 			},
 			{
 				label: 'f128',
 				kind: CompletionItemKind.Text,
-				data: 2
+				data: 'f128'
 			},
 			{
 				label: 'BFloat16',
 				kind: CompletionItemKind.Text,
-				data: 2
+				data: 'BFloat16'
 			},
 			{
 				label: 'String',
 				kind: CompletionItemKind.Text,
-				data: 4
+				data: 'string'
 			},
 			{
 				label: 'StringView',
 				kind: CompletionItemKind.Text,
-				data: 4
+				data: 'string'
 			},
 			{
 				label: 'i8',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'i8'
 			},
 			{
 				label: 'i16',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'i16'
 			},
 			{
 				label: 'i32',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'i32'
 			},
 			{
 				label: 'i64',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'i64'
 			},
 			{
 				label: 'i128',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'i128'
 			},
 			{
 				label: 'i256',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'i256'
 			},
 			{
 				label: 'u8',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'u8'
 			},
 			{
 				label: 'u16',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'u16'
 			},
 			{
 				label: 'u32',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'u32'
 			},
 			{
 				label: 'u64',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'u64'
 			},
 			{
 				label: 'u128',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'u128'
 			},
 			{
 				label: 'u256',
 				kind: CompletionItemKind.Text,
-				data: 1
+				data: 'u256'
 			},
 			// FUNCTIONS
 
 			{
 				label: 'if',
 				kind: CompletionItemKind.Text,
-				data: 6
+				data: 'if'
 			},
 			{
 				label: 'while',
 				kind: CompletionItemKind.Text,
-				data: 6
+				data: 'while'
 			},
 			{
 				label: 'then',
 				kind: CompletionItemKind.Text,
-				data: 6
+				data: 'then'
 			},
 			{
 				label: 'else',
 				kind: CompletionItemKind.Text,
-				data: 6
+				data: 'else'
 			},
 			{
 				label: 'Carbon',
@@ -336,7 +338,7 @@ connection.onCompletion(
 			{
 				label: 'fn',
 				kind: CompletionItemKind.Text,
-				data: 6
+				data: 'fn'
 			},
 			{
 				label: 'Slice',
@@ -346,12 +348,12 @@ connection.onCompletion(
 			{
 				label: 'in',
 				kind: CompletionItemKind.Text,
-				data: 6
+				data: 'in'
 			},
 			{
 				label: 'return',
 				kind: CompletionItemKind.Text,
-				data: 6
+				data: 'return'
 			},
 			{
 				label: 'let',
@@ -361,12 +363,12 @@ connection.onCompletion(
 			{
 				label: 'break',
 				kind: CompletionItemKind.Text,
-				data: 6
+				data: 'break'
 			},
 			{
 				label: 'continue',
 				kind: CompletionItemKind.Text,
-				data: 6
+				data: 'continue'
 			},
 			{
 				label: 'match',
@@ -380,6 +382,7 @@ connection.onCompletion(
 			},
 			{
 				label: 'abstract',
+				//the kind is markdown
 				kind: CompletionItemKind.Text,
 				data: 7
 			},
@@ -474,27 +477,136 @@ connection.onCompletionResolve(
 		if (item.data === 1) { // i numbers
 			item.detail = 'Carbon Signed Integer Type';
 			item.documentation = 'Contains a integer value. You can set the number of bits by using i16, i32, i64, i128, or i256.';
-		} else if (item.data === 2) { // f numbers
+		} else if (item.data === 'f16') { // f numbers
 			item.detail = 'Carbon Floating Point Type';
-			item.documentation = 'This is a floating point';
+			item.documentation = 'f16 is a 16-bit floating point type. You can assign values from -65504 to 65504 to it.';
 		} 
-		else if (item.data === 3) {
-			item.detail = 'Carbon Var';
-			item.documentation = 'A var declares a variable.';
+		else if (item.data === 'f32') { // f numbers
+			item.detail = 'Carbon Floating Point Type';
+			item.documentation = 'f32 is a 32-bit floating point type. You can assign values from -2147483648 to 2147483647 to it.';
 		}
-		else if (item.data === 4) {
+		else if (item.data === 'f64') { // f numbers
+			item.detail = 'Carbon Floating Point Type';
+			item.documentation = 'f64 is a 64-bit floating point type. You can assign values from -9223372036854775808 to 9223372036854775807 to it.';
+		}
+		else if (item.data === 'f128') { // f numbers
+			item.detail = 'Carbon Floating Point Type';
+			item.documentation = 'f128 is a 128-bit floating point type. You can assign values from -1.7*10^38 to 1.7*10^38 to it.';
+		}
+		else if (item.data === 'bFloat16') { // b numbers
+			item.detail = 'Carbon Boolean Type';
+			item.documentation = 'bFloat16 is a 16-bit floating point type. You can assign values from -65504 to 65504 to it.';
+		}
+		else if (item.data === 'i8') { // i numbers
+			item.detail = 'Carbon Signed Integer Type';
+			item.documentation = 'i8 is a 8-bit signed integer type. You can assign values from -128 to 127 to it.';
+		}
+		else if (item.data === 'i16') { // i numbers
+			item.detail = 'Carbon Signed Integer Type';
+			item.documentation = 'i16 is a 16-bit signed integer type. You can assign values from -32768 to 32767 to it.';
+		}
+		else if (item.data === 'i32') { // i numbers
+			item.detail = 'Carbon Signed Integer Type';
+			item.documentation = 'i32 is a 32-bit signed integer type. You can assign values from -2147483648 to 2147483647 to it.';
+		}
+		else if (item.data === 'i64') { // i numbers
+			item.detail = 'Carbon Signed Integer Type';
+			item.documentation = 'i64 is a 64-bit signed integer type. You can assign values from -9223372036854775808 to 9223372036854775807 to it.';
+		}
+		else if (item.data === 'i128') { // i numbers
+			item.detail = 'Carbon Signed Integer Type';
+			item.documentation = 'i128 is a 128-bit signed integer type. You can assign values from -1.7*10^38 to 1.7*10^38 to it.';
+		}
+		else if (item.data === 'i256') { // i numbers
+			item.detail = 'Carbon Signed Integer Type';
+			item.documentation = 'i256 is a 256-bit signed integer type. You can assign values from -2*2^256-1 to 2*2^256-1 to it.';
+		}
+		else if (item.data === 'u8') { // unsigned numbers
+			item.detail = 'Carbon Unsigned Integer Type';
+			item.documentation = 'u8 is a 8-bit unsigned integer type. You can assign values from 0 to 255 to it.';
+		}
+		else if (item.data === 'u16') { // unsigned numbers
+			item.detail = 'Carbon Unsigned Integer Type';
+			item.documentation = 'u16 is a 16-bit unsigned integer type. You can assign values from 0 to 65535 to it.';
+		}
+		else if (item.data === 'u32') { // unsigned numbers
+			item.detail = 'Carbon Unsigned Integer Type';
+			item.documentation = 'u32 is a 32-bit unsigned integer type. You can assign values from 0 to 4294967295 to it.';
+		}
+		else if (item.data === 'u64') { // unsigned numbers
+			item.detail = 'Carbon Unsigned Integer Type';
+			item.documentation = 'u64 is a 64-bit unsigned integer type. You can assign values from 0 to 18446744073709551615 to it.';
+		}
+		else if (item.data === 'u128') { // unsigned numbers
+			item.detail = 'Carbon Unsigned Integer Type';
+			item.documentation = 'u128 is a 128-bit unsigned integer type. You can assign values from 0 to 1.7^38-1 to it.';
+		}
+		else if (item.data === 'u256') { // unsigned numbers
+			item.detail = 'Carbon Unsigned Integer Type';
+			item.documentation = 'u256 is a 256-bit unsigned integer type. You can assign values from 0 to 2^256-1 to it.';
+		}
+		// functions
+		else if (item.data === 'if') {
+			item.detail = 'Carbon If Function';
+			item.documentation = 'if is a function that returns a boolean value. It takes two parameters, a boolean value and a function. If the boolean value is true, the function will be executed. Otherwise, the function will not be executed.';
+		}
+		else if (item.data === 'while') {
+			item.detail = 'Carbon While Function';
+			item.documentation = 'while is a function that returns a boolean value. It takes two parameters, a boolean value and a function. If the boolean value is true, the function will be executed while it remains true. Otherwise, the function will not be executed.';
+		}
+		else if (item.data === 'then') {
+			item.detail = 'Carbon Then Function';
+			item.documentation = 'then is a function that executes if its corresponding if statement is true. It takes no parameters.';
+		}
+		else if (item.data === 'else') {
+			item.detail = 'Carbon Else Function';
+			item.documentation = 'else is a function that executes if its corresponding if statement is false. It takes no parameters.';
+		}
+		else if (item.data === 'fn') {
+			item.detail = 'Carbon Function';
+			item.documentation = 'A function is a block of code that can be called from other code. Functions can be used to create custom logic, to handle events, or to create reusable components. (Carbon)';
+		}
+		else if (item.data === 'in') {
+			item.detail = 'Carbon In';
+			item.documentation = "'in' returns true if the provided value is in the provided list";
+		}
+		else if (item.data === 'return') {
+			item.detail = 'Carbon Return';
+			item.documentation = 'return is a function that returns a value. It takes one parameter, a value. It is used to return a value from a function.';
+		}
+		else if (item.data === 'break') {
+			item.detail = 'Carbon Break';
+			item.documentation = 'The break keyword is used to break out of a loop.';
+		}
+		else if (item.data === 'continue') {
+			item.detail = 'Carbon Continue';
+			item.documentation = 'The continue keyword is used to continue to the next iteration of a loop.';
+		}
+		else if (item.data === 'true') {
+			item.detail = 'Carbon True';
+			item.documentation = 'true is a boolean value that is true.';
+		}
+		else if (item.data === 'false') {
+			item.detail = 'Carbon False';
+			item.documentation = 'false is a boolean value that is false.';
+		}
+		else if (item.data === 'var') {
+			item.detail = 'Carbon Var';
+			item.documentation = 'This is a variable. A variable is a non-constant value that can be assigned and reassigned a value.';
+		}
+		else if (item.data === 'string') {
 			item.detail = 'Carbon String';
-			item.documentation = 'A String is a byte sequence treated as containing UTF-8 encoded text. A StringView is a read only variation of a String.';
+			item.documentation = 'A String is a byte sequence treated as containing UTF-8 encoded text. A StringView is a read only variation of a String, used to access the contents of a String without copying the String’s contents.';
 		}
 		else if (item.data === 5) {
 			item.detail = 'Carbon Floating Point Type';
 			item.documentation = 'Contains a floating point value that rounds-to-nearest. You can set the number of bits by using f16, f32, f64, and f128. BFloat16 is also supported.';
 		}else if (item.data === 6) {
 			item.detail = 'Carbon Function';
-			item.documentation = 'Function - Description to be added.';
+			item.documentation = 'A function is a block of code that can be called from other code. Functions can be used to create custom logic, to handle events, or to create reusable components.';
 		}else if (item.data === 7) {
-			item.detail = 'Carbon Class Modifier';
-			item.documentation = 'Class Modifier - Description to be added.';
+			item.detail = 'Class Modifier';
+			item.documentation = 'A class modifier is a modifier that can be applied to a class. It can be used to modify the class’s behavior.';		
 		}
 		return item;
 	}
---
+++ testing/README.md
@@ -0,0 +1,1 @@
+The files in this folder are used for testing Intellisense in other languages.
